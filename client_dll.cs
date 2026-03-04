754e87fa86803b45231edbf744f79ad0d2f1a8bb
TafoFps <141164248+TafoFps@users.noreply.github.com>
2026-03-05 02:45:23 +0300

    Update client_dll.cs

--- client_dll.cs
+++ client_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-02-13 11:47:04.364649100 UTC
+// 2026-03-04 23:43:39.915197700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: client.dll
