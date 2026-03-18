788d9cc80bba09830b2bb20a1641c2c9eb5b7899
TafoFps <141164248+TafoFps@users.noreply.github.com>
2026-03-19 02:17:20 +0300

    Add files via upload

--- engine2_dll.cs
+++ engine2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-13 21:37:36.524012 UTC
+// 2026-03-18 23:16:22.648743300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: engine2.dll
